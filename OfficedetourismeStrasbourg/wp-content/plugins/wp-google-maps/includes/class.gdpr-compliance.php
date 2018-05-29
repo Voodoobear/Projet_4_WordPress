8490dd7fb4a0e332606817e68f4e7ed5784cd9d5
Voodoobear <s.chalain@gmail.com>
2018-05-29 16:28:03 +0200

    Site remis en place et contenu ajouté. Début de mises en forme

---
+++ OfficedetourismeStrasbourg/wp-content/plugins/wp-google-maps/includes/class.gdpr-compliance.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace WPGMZA;
+
+class GDPRCompliance
+{
+	public function __construct()
+	{
+		add_filter('wpgmza_global_settings_tabs', array($this, 'onGlobalSettingsTabs'));
+		add_filter('wpgmza_global_settings_tab_content', array($this, 'onGlobalSettingsTabContent'));
+		
+		add_filter('wpgmza_plugin_get_default_settings', array($this, 'onPluginGetDefaultSettings'));
+		
+		add_action('wp_ajax_wpgmza_gdpr_privacy_policy_notice_dismissed', array($this, 'onPrivacyPolicyNoticeDismissed'));
+	}
+	
+	public function onPluginGetDefaultSettings($settings)
+	{
+		return array_merge($settings, array(
+			'wpgmza_gdpr_enabled'		=> 1,
+			'wpgmza_gdpr_notice'		=> apply_filters('wpgmza_gdpr_notice',
+											__('I agree for my personal data, provided via submission through \'User Generated Markers\' where applicable, \'WP Google Maps\' Live Tracking app, where applicable, to be processed by {COMPANY_NAME}.
+		
+I agree for my personal data, provided via map API calls, to be processed by the API provider, for the purposes of geocoding (converting addresses to coordinates), reverse geocoding and generating directions.
+
+WP Google Maps uses jQuery DataTables to display sortable, searchable tables, such as that seen in the Advanced Marker Listing and on the Map Edit Page. jQuery DataTables in certain circumstances uses a cookie to save and later recall the "state" of a given table - that is, the search term, sort column and order and current page. This data is help in local storage and retained until this is cleared manually. No libraries used by WP Google Maps transmit this information.
+
+Some visual components of WP Google Maps use 3rd party libraries which are loaded over the network. At present the libraries are Google Maps, Open Street Map, jQuery DataTables and FontAwesome. When loading resources over a network, the 3rd party server will receive your IP address and User Agent string amongst other details. Please refer to the Privacy Policy of the respective libraries for details on how they use data and the process to exercise your rights under the GDPR regulations.
+
+When using the User Generated Marker addon, data will be stored indefinitiely for the following purpose(s): {RETENTION_PURPOSE}'), 'wp-google-maps'),
+			
+			'wpgmza_gdpr_retention_purpose' => 'presenting the data you have submitted on the map.'
+		));
+	}
+	
+	public function onPrivacyPolicyNoticeDismissed()
+	{
+		$wpgmza_other_settings = get_option('WPGMZA_OTHER_SETTINGS');
+		$wpgmza_other_settings['privacy_policy_notice_dismissed'] = true;
+		update_option('WPGMZA_OTHER_SETTINGS', $wpgmza_other_settings);
+		
+		wp_send_json(array(
+			'success' => 1
+		));
+		exit;
+	}
+	
+	protected function getSettingsTabContent()
+	{
+		$wpgmza_other_settings = get_option('WPGMZA_OTHER_SETTINGS');
+		
+		$document = new DOMDocument();
+		$document->loadPHPFile(plugin_dir_path(__DIR__) . 'html/gdpr-compliance-settings.html.php');
+		$document->populate($wpgmza_other_settings);
+		
+		return $document;
+	}
+	
+	public function getNoticeHTML()
+	{
+		$wpgmza_other_settings = get_option('WPGMZA_OTHER_SETTINGS');
+		
+		if(!$wpgmza_other_settings || empty($wpgmza_other_settings['wpgmza_gdpr_notice']))
+			return '';
+		
+		$html = $wpgmza_other_settings['wpgmza_gdpr_notice'];
+		
+		$html = preg_replace('/{COMPANY_NAME}/i', $wpgmza_other_settings['wpgmza_gdpr_company_name'], $html);
+		$html = preg_replace('/{RETENTION_PERIOD}/i', $wpgmza_other_settings['wpgmza_gdpr_retention_period_days'], $html);
+		$html = preg_replace('/{RETENTION_PURPOSE}/i', $wpgmza_other_settings['wpgmza_gdpr_retention_purpose'], $html);
+		
+		$html = '<input type="checkbox" name="wpgmza_ugm_gdpr_consent" required/> ' . $html;
+		
+		$html = apply_filters('wpgmza_gdpr_notice_html', $html);
+		
+		return $html;
+	}
+	
+	public function getPrivacyPolicyNoticeHTML()
+	{
+		global $wpgmza;
+		
+		if(!empty($wpgmza->settings->privacy_policy_notice_dismissed))
+			return '';
+		
+		return "
+			<div id='wpgmza-gdpr-privacy-policy-notice' class='notice notice-info is-dismissible'>
+				<p>" . __('In light of recent EU GDPR regulation, we strongly recommend reviewing the <a target="_blank" href="https://www.wpgmaps.com/privacy-policy">WP Google Maps Privacy Policy</a>', 'wp-google-maps') . "</p>
+			</div>
+			";
+	}
+	
+	public function onGlobalSettingsTabs($input)
+	{
+		return $input . "<li><a href=\"#wpgmza-gdpr-compliance\">".__("GDPR Compliance","wp-google-maps")."</a></li>";
+	}
+	
+	public function onGlobalSettingsTabContent()
+	{
+		$document = $this->getSettingsTabContent();
+		return $document->saveInnerBody();
+	}
+	
+	public function onPOST()
+	{
+		$document = $this->getSettingsTabContent();
+		$document->populate($_POST);
+		
+		$wpgmza_other_settings = get_option('WPGMZA_OTHER_SETTINGS');
+		if(!$wpgmza_other_settings)
+			$wpgmza_other_settings = array();
+		
+		foreach($document->querySelectorAll('input, select, textarea') as $input)
+		{
+			
+			$name = $input->getAttribute('name');
+			
+			if(!$name)
+				continue;
+			
+			switch($input->getAttribute('type'))
+			{
+				
+				case 'checkbox':
+					if(isset($wpgmza_other_settings[$name]))
+						unset($wpgmza_other_settings[$name]);
+					else
+						$wpgmza_other_settings[$name] = 1;
+					break;
+				
+				default:
+					$wpgmza_other_settings[$name] = stripslashes( $input->getValue() );
+					break;
+				
+			}
+			
+		}
+		
+		update_option('WPGMZA_OTHER_SETTINGS', $wpgmza_other_settings);
+	}
+}
+
+$wpgmzaGDPRCompliance = new GDPRCompliance();
---
+++ OfficedetourismeStrasbourg/wp-content/plugins/wp-google-maps/includes/class.google-maps-api-loader.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace WPGMZA;
+
+if(class_exists('WPGMZA\\GoogleMapsAPILoader'))
+	return;
+
+class GoogleMapsAPILoader
+{
+	private static $googleAPILoadCalled = false;
+	private static $settings;
+	
+	public function __construct()
+	{
+		if(empty(GoogleMapsAPILoader::$settings))
+		{
+			global $wpgmza;
+			GoogleMapsAPILoader::$settings = (array)$wpgmza->settings;
+		}
+	}
+	
+	public static function _createInstance()
+	{
+		return new GoogleMapsAPILoader();
+	}
+	
+	public static function createInstance()
+	{
+		return static::_createInstance();
+	}
+	
+	/**
+	 * Gets the parameters to be sent to the Google Maps API load call
+	 * @return array
+	 */
+	protected function getGoogleMapsAPIParams()
+	{
+		// Locale
+		$locale = get_locale();
+		$suffix = '.com';
+		
+		switch($locale)
+		{
+			case 'he_IL':
+				// Hebrew correction
+				$locale = 'iw';
+				break;
+			
+			case 'zh_CN':
+				// Chinese integration
+				$suffix = '.cn';
+				break;
+		}
+		
+		
+		$locale = substr($locale, 0, 2);
+		
+		// Default params for google maps
+		$params = array(
+			'v' 		=> '3.31',
+			'language'	=> $locale,
+			'suffix'	=> $suffix
+		);
+		
+		// API Key
+		$key = get_option('wpgmza_google_maps_api_key');
+		
+		if($key)
+			$params['key'] = $key;
+		else if(is_admin())
+			$params['key'] = get_option('wpgmza_temp_api');
+		
+		// API Version
+		$settings = (array)GoogleMapsAPILoader::$settings;
+		
+		if(!empty($settings['wpgmza_api_version']))
+			$params['v'] = $settings['wpgmza_api_version'];
+		
+		// Libraries
+		$libraries = array('geometry', 'places', 'visualization');
+		$params['libraries'] = implode(',', $libraries);
+		
+		$params = apply_filters( 'wpgmza_google_maps_api_params', $params );
+		
+		return $params;
+	}
+	
+	/**
+	 * This function loads the Google API if it hasn't been called already
+	 * @return void
+	 */
+	public function registerGoogleMaps()
+	{
+		global $post;
+		
+		$settings = (array)GoogleMapsAPILoader::$settings;
+		
+		if(GoogleMapsAPILoader::$googleAPILoadCalled)
+			return;
+		
+		$params = $this->getGoogleMapsAPIParams();
+		
+		$suffix = $params['suffix'];
+		unset($params['suffix']);
+		
+		$url = '//maps.google' . $suffix . '/maps/api/js?' . http_build_query($params);
+		
+		wp_register_script('wpgmza_api_call', $url);
+		
+		// Are we always enqueuing?
+		if(!empty($settings['wpgmza_load_engine_api_condition']) && $settings['wpgmza_load_engine_api_condition'] == 'always')
+			$this->enqueueGoogleMaps();
+		
+		// Are we always enqueuing on this page?
+		if($post && $this->isPageIncluded($post->ID))
+			$this->enqueueGoogleMaps();
+		
+		GoogleMapsAPILoader::$googleAPILoadCalled = true;
+		
+		// Block other plugins from including the API
+		add_filter('script_loader_tag', array($this, 'preventOtherGoogleMapsTag'), 9999999, 3);
+	}
+	
+	public function enqueueGoogleMaps()
+	{
+		wp_enqueue_script('wpgmza_api_call');
+	}
+	
+	public function isPageIncluded($page_id)
+	{
+		global $post;
+		$settings = (array)GoogleMapsAPILoader::$settings;
+		
+		if(empty($settings['wpgmza_always_include_engine_api_on_pages']))
+			return false;
+		
+		if(!preg_match_all('/\d+/', $settings['wpgmza_always_include_engine_api_on_pages'], $m))
+			return false;
+		
+		if(empty($m[0]))
+			return false;
+		
+		$page_ids = $m[0];
+		
+		return (array_search($page_id, $page_ids) !== false);
+	}
+	
+	public function isPageExcluded($page_id)
+	{
+		$settings = (array)GoogleMapsAPILoader::$settings;
+		
+		if(empty($settings['wpgmza_always_exclude_engine_api_on_pages']))
+			return false;
+		
+		if(!preg_match_all('/\d+/', $settings['wpgmza_always_exclude_engine_api_on_pages'], $m))
+			return false;
+			
+		if(empty($m[0]))
+			return false;
+		
+		$page_ids = $m[0];
+			
+		return (array_search($page_id, $page_ids) !== false);
+	}
+	
+	public function isIncludeAllowed(&$reason=null)
+	{
+		global $wpgmza;
+		global $post;
+		
+		$settings = (array)$wpgmza->settings;
+		
+		if(!empty($settings['wpgmza_settings_remove_api']))
+		{
+			$reason = 'Remove API checked in settings';
+			return false;
+		}
+		
+		if(empty($settings['wpgmza_maps_engine']) || $settings['wpgmza_maps_engine'] != 'google-maps')
+		{
+			$reason = 'Engine is not google-maps';
+			return false;
+		}
+		
+		if($post)
+		{
+			if($this->isPageIncluded($post->ID))
+			{
+				$reason = 'Page is explicitly included in settings';
+				return true;
+			}
+			
+			if($this->isPageExcluded($post->ID))
+			{
+				$reason = 'Page is explicitly excluded in settings';
+				return false;
+			}
+		}
+			
+		if(!empty($settings['wpgmza_load_engine_api_condition']))
+			switch($settings['wpgmza_load_engine_api_condition'])
+			{
+				case 'never':
+					$reason = 'Never load API chosen in settings';
+					return false;
+					break;
+					
+				case 'only-front-end':
+					$reason = 'Load API front end only chosen in settings';
+					return !is_admin();
+					break;
+					
+				case 'only-back-end':
+					$reason = 'Load API back end only chosen in settings';
+					return is_admin();
+					break;
+				
+				default:
+					break;
+			}
+		
+		$reason = 'Enqueue is allowed';
+		return true;
+	}
+	
+	public function preventOtherGoogleMapsTag($tag, $handle, $src)
+	{
+		if(preg_match('/maps\.google/i', $src))
+		{
+			if(!$this->isIncludeAllowed($reason))
+			{
+				echo "<script>var wpgmza_api_not_enqueued_reason = '$reason';</script>";
+				return '';
+			}
+			
+			else if($handle != 'wpgmza_api_call')
+				return '';
+			
+			if(!preg_match('/\?.+$/', $src))
+				return str_replace($src, $src . '?' . http_build_query($this->getGoogleMapsAPIParams()), $tag);
+		}
+
+		return $tag;
+	}
+	
+	public function getSettingsHTML()
+	{
+		// Load our subclass of PHPs DOMDocument, for the populate function
+		require_once(plugin_dir_path(__FILE__) . 'class.dom-document.php');
+		
+		// Load HTML
+		$document = new DOMDocument();
+		$document->loadPHPFile(plugin_dir_path(__DIR__) . 'html/google-maps-api-settings.html.php');
+		
+		// Populate options. This is a quick way to put key => value array/object values into elements with "name" matching "key"
+		$document->populate(GoogleMapsAPILoader::$settings);
+		
+		return $document->saveInnerBody();
+	}
+	
+}
